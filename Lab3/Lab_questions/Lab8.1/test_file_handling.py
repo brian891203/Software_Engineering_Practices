237e201d6275e405bfc5c482210fd867718f104c
Brian <brian891203@gmail.com>
2025-03-10 21:57:23 +0800

    Add: Upload and complete Lab 8.1 and Lab 8.3

--- Lab3/Lab_questions/Lab8.1/test_file_handling.py
+++ Lab3/Lab_questions/Lab8.1/test_file_handling.py
@@ -1,20 +1,23 @@
 # test_file_handling.py
 
-import pytest
 import os
+
+import pytest
 from file_handler import FileHandler
+
 
 # Define a fixture to set up and tear down a temporary file
 @pytest.fixture
 def temp_file(tmpdir):
     # please open a tempory file with its name "filename" 
-    #.......
+    temp = tmpdir.join("filename")
     
     # This is where the test function runs
-    #........
+    temp.write("")
+    yield str(temp)
     
     # please close the tempory file and remove it from the file systems
-    #.......
+    temp.remove()
 
 # Test function that uses the temp_file fixture
 def test_file_handling(temp_file):
