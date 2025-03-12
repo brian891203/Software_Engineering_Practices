d956c3112e4cc53f9fac179a62a9a792ba7e35f8
Brian <brian891203@gmail.com>
2025-03-12 08:10:59 -0700

    Feat: [HW4] (reach HW requirements for executig the different process fully randomly(3!))

--- HW4/src/p2.c
+++ HW4/src/p2.c
@@ -3,13 +3,24 @@
 
 int main(void) {
     int i = 0 ;
-    int semP2, semP3;
+    int semP1, semP2, semP3;
     // *** please insert proper semaphore initialization here
+    semP1 = get_sem(".",'P1');
     semP2 = get_sem(".",'P2');
     semP3 = get_sem(".",'P3');
 
+    if (semP1==-1 && semP2==-1 && semP3==-1) {
+        semP1 = create_sem(".",'P1',1);
+        semP2 = create_sem(".",'P2',0);
+        semP3 = create_sem(".",'P3',0);
+    }
+
     do {
         // *** this is where you should place semaphore
+        while(semP1 && !semP2 && !semP3) {
+            V(semP1);
+        }
+
         P(semP2);
 
         printf("P222222222 is here\n"); i++ ;
--- HW4/src/p3.c
+++ HW4/src/p3.c
@@ -5,12 +5,22 @@ int main(void) {
     int i = 0 ;
     int semP1, semP2, semP3;
     // *** please insert proper semaphore initialization here
-    semP3 = get_sem(".",'P3');
-    semP2 = get_sem(".",'P2');
     semP1 = get_sem(".",'P1');
+    semP2 = get_sem(".",'P2');
+    semP3 = get_sem(".",'P3');
+
+    if (semP1==-1 && semP2==-1 && semP3==-1) {
+        semP1 = create_sem(".",'P1',1);
+        semP2 = create_sem(".",'P2',0);
+        semP3 = create_sem(".",'P3',0);
+    }
 
     do {
         // *** this is where you should place semaphore
+        while(semP1 && !semP2 && !semP3) {
+            V(semP1);
+        }
+
         // check for semP2 == 0 and semP3 == 0
         while (!semP2 && !semP3) {
             V(semP2);
